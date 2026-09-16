71e1e5bffcd9d30200252bae67d676b485cb42ca
Petronilha <petronilhadaniel@gmail.com>
2026-09-16 12:06:43 -0300

    Created 'delete_task' method in app.py for delete a specific task

--- app.py
+++ app.py
@@ -45,16 +45,31 @@ def update_task(id):
   for t in tasks:
     if t.id == id:
       task = t
-  print(task)
+
   if task == None:
     return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
   data = request.get_json()
-  task.title = data['title']
+  task.title = data['title'] 
   task.description = data['description']
   task.completed = data['completed']
-  print(task)
+
   return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+# Delete task
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+  
+  if task == None:
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+  
+  tasks.remove(task)
+  return jsonify({"message": "Tarefa deletada com sucesso "})
 
 
 if __name__ == "__main__":
