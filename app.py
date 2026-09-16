0c1b8303969b9a03c79790ba5c78fb17db1a58ac
Petronilha <petronilhadaniel@gmail.com>
2026-09-16 11:13:15 -0300

    Created 'get_task' method in app.py for get a specific task

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 tasks = []
 task_id_control = 1
 
+# Create a task
 @app.route('/tasks', methods=['POST'])
 def create_task():
   global task_id_control
@@ -19,14 +20,23 @@ def create_task():
   print(tasks)
   return jsonify({"message": "Nova tarefa criada com sucesso"})
 
+# Get all tasks 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
   task_list = [task.to_dict() for task in tasks]
   output = {
     "tasks": task_list,
-    "total_tasks": len(tasks)
+    "total_tasks": len(tasks )
   }
   return jsonify(output)
+
+# Get a specific task  
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+  for t in tasks:
+    if t.id == id:
+      return jsonify(t.to_dict())
+  return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
 if __name__ == "__main__":
   app.run(debug=True)
